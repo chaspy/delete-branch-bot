90d2749a5d534628badfb736317d001510cfb998
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-18 02:13:39 +0900

    Re-define struct to get necessary items

--- main.go
+++ main.go
@@ -10,15 +10,22 @@ type MyResponse struct {
 }
 
 type PullRequestEvent struct {
-	Action string `json:"action"`
-	Number int    `json:"number"`
+	Action      string `json:"action"`
+	PullRequest struct {
+		Head struct {
+			Ref  string `json:"ref"`
+			Repo struct {
+				FullName string `json:"full_name"`
+			} `json:"repo"`
+		} `json:"head"`
+	} `json:"pull_request"`
 }
 
 func delete_branch(event PullRequestEvent) (MyResponse, error) {
 	fmt.Println("log start")
 	fmt.Println(event)
 	fmt.Println("log end")
-	return MyResponse{Message: fmt.Sprintf("PullRequest action is %s!!", event.Action)}, nil
+	return MyResponse{Message: fmt.Sprintf("PullRequest action is %s!! repo is %s!! branch name is %s!!", event.Action, event.PullRequest.Head.Ref, event.PullRequest.Head.Repo.FullName)}, nil
 }
 
 func main() {
