0c4b0829d5ce83c1a2763ee2501e755131c755ba
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-17 19:30:45 +0900

    Add lambda.go

---
+++ lambda.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"What is your name?"`
+}
+
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
+
+func hello(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("Hello %s!!", event.Name)}, nil
+}
+
+func main() {
+	lambda.Start(hello)
+}
