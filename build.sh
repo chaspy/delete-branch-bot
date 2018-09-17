27e491b8c81a7001c38509fd00b4ab361e98304f
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-18 01:20:46 +0900

    Change file name

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm -f ./delete-branch
+rm -f handler.zip
 GOOS=linux GOARCH=amd64 go build -o delete-branch
 zip handler.zip ./delete-branch
--- main.go
+++ main.go
@@ -6,20 +6,14 @@ import (
 	"github.com/chaspy/delete-branch/model"
 )
 
-type MyEvent struct {
-	Name string `json:"What is your name?"`
-}
-
 type MyResponse struct {
 	Message string `json:"Answer:"`
 }
 
-func hello(event MyEvent) (MyResponse, error) {
-  data := new(model.PullRequestEvent)
-  var _ = data // avoid build error
-	return MyResponse{Message: fmt.Sprintf("Hello %s!!", event.Name)}, nil
+func delete_branch(event model.PullRequestEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("PullRequest action is %s!!", event.Payload.Action)}, nil
 }
 
 func main() {
-	lambda.Start(hello)
+	lambda.Start(delete_branch)
 }
