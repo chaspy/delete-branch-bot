a69a5cef063e2accd860a91bf0e5442589227d0c
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-09-26 22:49:52 +0900

    Remove implementation comment

--- main.go
+++ main.go
@@ -496,27 +496,6 @@ func delete_branch(event PullRequestEvent) (MyResponse, error) {
 	var message MyResponse
 	var errorType error
 
-	// hi @kamontia! I checked this lambda function works from GitHub-App via API-Gateway.
-	// I leave the hint to implement we realy want to do!
-	//
-	// currently, I already cache PullRequestEvent from GitHub,
-	// and Got values we need.
-	//
-	// action: event.Action (i.e closed or merged)
-	// repo  : event.PullRequest.Head.Repo.FullName (i.e chaspy/delete-branch)
-	// branch: event.PullRequest.Head.Ref (i.e test-branch)
-	// ref https://github.com/chaspy/test-delete-branch/pull/1
-	// (I used this PR to test)
-	//
-	// So what you should implement is very simple!
-	//
-	// if action is "closed" or "merged" then
-	//  delete branch(repo,branch)
-	// end
-	// return nothing(204 No Content)
-	//
-	// If there is concern, it will be part of certification.
-	// ref: https://github.com/github/platform-samples/blob/b047a807dd43a3f76c2cbf0e85af3ffadeb2b880/api/ruby/building-your-first-github-app/advanced_server.rb#L24-L44
 	if event.Action == "closed" || event.Action == "merged" {
 		fmt.Println(event.Action)
 
