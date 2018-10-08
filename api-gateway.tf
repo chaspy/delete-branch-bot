b1ab3adcbf70f50e32372b9d8c7943d9cb4724ae
kondo takeshi <chaspy@users.noreply.github.com>
2018-10-09 01:14:09 +0900

    Remove internal resource
    it was not required internal resource.

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,5 +1,5 @@
-resource "aws_api_gateway_rest_api" "delete-branch" {
-  name        = "delete-branch-bot"
+resource "aws_api_gateway_rest_api" "delete_branch_bot" {
+  name        = "delete_branch_bot"
   description = "Delete merged branch when Pull Request merged"
 
   endpoint_configuration {
@@ -7,23 +7,23 @@ resource "aws_api_gateway_rest_api" "delete-branch" {
   }
 }
 
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = "${aws_api_gateway_rest_api.delete_branch_bot.id}"
+  resource_id   = "${aws_api_gateway_rest_api.delete_branch_bot.root_resource_id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
 resource "aws_api_gateway_deployment" "prod" {
   depends_on = [
-    "aws_api_gateway_integration.delete-branch",
+    "aws_api_gateway_integration.delete_branch_bot",
   ]
-  rest_api_id = "${aws_api_gateway_rest_api.delete-branch.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.delete_branch_bot.id}"
   stage_description = "production"
   stage_name = "prod"
 }
 
-# /post
-resource "aws_api_gateway_resource" "post" {
-  parent_id = "${aws_api_gateway_rest_api.delete-branch.root_resource_id}"
-  path_part = "/"
-  rest_api_id = "${aws_api_gateway_rest_api.delete-branch.id}"
-}
-
-resource "aws_api_gateway_integration" "delete-branch" {
+resource "aws_api_gateway_integration" "delete_branch_bot" {
   http_method = "${aws_api_gateway_method.post.http_method}"
   integration_http_method = "POST"
   request_templates = {
@@ -34,30 +34,23 @@ resource "aws_api_gateway_integration" "delete-branch" {
 }
 EOF
   }
-  resource_id = "${aws_api_gateway_resource.post.id}"
-  rest_api_id = "${aws_api_gateway_rest_api.delete-branch.id}"
+  resource_id = "${aws_api_gateway_rest_api.delete_branch_bot.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.delete_branch_bot.id}"
   type = "AWS"
   uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${var.aws_account_id}:function:${aws_lambda_function.delete_branch_bot.function_name}/invocations"
 }
 
-resource "aws_api_gateway_method" "post" {
-  authorization = "NONE"
-  http_method = "POST"
-  resource_id = "${aws_api_gateway_resource.post.id}"
-  rest_api_id = "${aws_api_gateway_rest_api.delete-branch.id}"
-}
-
 resource "aws_api_gateway_method_response" "200" {
   http_method = "${aws_api_gateway_method.post.http_method}"
-  resource_id = "${aws_api_gateway_resource.post.id}"
-  rest_api_id = "${aws_api_gateway_rest_api.delete-branch.id}"
+  resource_id = "${aws_api_gateway_rest_api.delete_branch_bot.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.delete_branch_bot.id}"
   status_code = "200"
 }
 
 resource "aws_api_gateway_integration_response" "post_200" {
-  depends_on = ["aws_api_gateway_integration.delete-branch"]
+  depends_on = ["aws_api_gateway_integration.delete_branch_bot"]
   http_method = "${aws_api_gateway_method.post.http_method}"
-  resource_id = "${aws_api_gateway_resource.post.id}"
-  rest_api_id = "${aws_api_gateway_rest_api.delete-branch.id}"
+  resource_id = "${aws_api_gateway_rest_api.delete_branch_bot.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.delete_branch_bot.id}"
   status_code = "${aws_api_gateway_method_response.200.status_code}"
 }
