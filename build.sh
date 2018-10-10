509281c6311af1771347f19d82796690064bd5ef
kondo takeshi <chaspy@users.noreply.github.com>
2018-10-10 22:56:23 +0900

    Upload handler.zip to AWS lambda when building

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ rm -f ./delete-branch
 rm -f handler.zip
 GOOS=linux GOARCH=amd64 go build -o delete-branch
 zip handler.zip ./delete-branch ./private-key.pem
+aws lambda update-function-code \
+  --function-name delete_branch_bot \
+  --zip-file fileb://handler.zip \
+  --region ap-northeast-1
